ed8b57b21120562d599c969dc6be5e86c713d9fb
jackyga29 <jgallegosalcala29@unm.edu>
2024-02-28 13:11:39 -0700

    physics of player

--- PizzaGame/Assets/PlayerMovement.cs
+++ PizzaGame/Assets/PlayerMovement.cs
@@ -13,6 +13,9 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown("space"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 7, 0);
+        }
     }
 }
